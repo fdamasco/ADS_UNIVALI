4a5f4b2dc856a6d2e16dbe7510175d730527420f
Fernanda <81257522+fdamasco@users.noreply.github.com>
2021-04-17 14:39:22 -0300

    Update CalculadoraC.c

--- CalculadoraC.c
+++ CalculadoraC.c
@@ -1,53 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <locale.h> //Biblioteca para inserir acentuaÁ„o
-#include <math.h> //Biblioteca matem·tica para possibilitar a funÁ„o pow
-#include <string.h> //Biblioteca para habilitar funÁ„o strlen para saber o tamanho de uma string
+#include <locale.h> //Biblioteca para inserir acentua√ß√£o
+#include <math.h> //Biblioteca matem√°tica para possibilitar a fun√ß√£o pow
+#include <string.h> //Biblioteca para habilitar fun√ß√£o strlen para saber o tamanho de uma string
 
-void conversaoDecBin(int num_decimal); //FunÁ„o de convers„o de decimal para bin·rio
-void conversaoBinDec(char num_binario[64]); //FunÁ„o de convers„o de bin·rio em decimal
+void conversaoDecBin(int num_decimal); //Fun√ß√£o de convers√£o de decimal para bin√°rio
+void conversaoBinDec(char num_binario[64]); //Fun√ß√£o de convers√£o de bin√°rio em decimal
 
-//DeclaraÁ„o das vari·veis globais
+//Declara√ß√£o das vari√°veis globais
 int num_decimal, bin_decimal, opcao, i, j;
 int dec_binario[64];
 char num_binario[64];
 
 int main()
 {
-setlocale(LC_ALL, "Portuguese"); //Habilita acentuaÁ„o
+setlocale(LC_ALL, "Portuguese"); //Habilita acentua√ß√£o
 
-do{ //LaÁo de repetiÁ„o do menu
+do{ //La√ßo de repeti√ß√£o do menu
 
     // MENU DA CALCULADORA
-   printf("\tCALCULADORA DE CONVERS√O ENTRE SISTEMAS OPERACIONAIS!\n");
-   printf("\tESCOLHA A OP«√O DE CONVERS√O:\n");
-   printf("\nDigite [1] para converter um n˙mero Decimal em Bin·rio;\n");
-   printf("Digite [2] para converter um n˙mero Bin·rio em Decimal; ou\n");
+   printf("\tCALCULADORA DE CONVERS√ÉO ENTRE SISTEMAS OPERACIONAIS!\n");
+   printf("\tESCOLHA A OP√á√ÉO DE CONVERS√ÉO:\n");
+   printf("\nDigite [1] para converter um n√∫mero Decimal em Bin√°rio;\n");
+   printf("Digite [2] para converter um n√∫mero Bin√°rio em Decimal; ou\n");
    printf("Digite [3] para sair.\n");
-   printf("Opc„o escolhida: ");
-   scanf("%i", &opcao); //Armazena a vari·vel OPCAO
-   switch(opcao) { //Estrutura de seleÁ„o switch
+   printf("Opc√£o escolhida: ");
+   scanf("%i", &opcao); //Armazena a vari√°vel OPCAO
+   switch(opcao) { //Estrutura de sele√ß√£o switch
 
-       case 1: //OpÁ„o 1: convers„o de decimal para bin·rio
-           printf("\nA opc„o escolhida foi: %i - Convers„o de n˙mero Decimal em Bin·rio.\n", opcao);
-           printf("\nDigite um n˙mero inteiro e positivo em Decimal:\n");
-           scanf("%d", &num_decimal); //Armazena a vari·vel num_decimal
-           printf("N˙mero Bin·rio: \n");
-           conversaoDecBin(num_decimal); //Chamada da funÁ„o de convers„o decimal-bin·rio
+       case 1: //Op√ß√£o 1: convers√£o de decimal para bin√°rio
+           printf("\nA opc√£o escolhida foi: %i - Convers√£o de n√∫mero Decimal em Bin√°rio.\n", opcao);
+           printf("\nDigite um n√∫mero inteiro e positivo em Decimal:\n");
+           scanf("%d", &num_decimal); //Armazena a vari√°vel num_decimal
+           printf("N√∫mero Bin√°rio: \n");
+           conversaoDecBin(num_decimal); //Chamada da fun√ß√£o de convers√£o decimal-bin√°rio
            break;
 
-       case 2: //OpÁ„o 2: convers„o de bin·rio para decimal
-           printf("\nA opc„o escolhida foi: %i - Convers„o de n˙mero Bin·rio em Decimal.\n", opcao);
-           printf("\nDigite um n˙mero inteiro e positivo em Bin·rio:\n");
-           scanf("%s", &num_binario); //Armazena a vari·vel num_binario
-           conversaoBinDec(num_binario); //Chamada da funÁ„o de convers„o bin·rio-decimal
+       case 2: //Op√ß√£o 2: convers√£o de bin√°rio para decimal
+           printf("\nA opc√£o escolhida foi: %i - Convers√£o de n√∫mero Bin√°rio em Decimal.\n", opcao);
+           printf("\nDigite um n√∫mero inteiro e positivo em Bin√°rio:\n");
+           scanf("%s", &num_binario); //Armazena a vari√°vel num_binario
+           conversaoBinDec(num_binario); //Chamada da fun√ß√£o de convers√£o bin√°rio-decimal
            break;
 
-       case 3: //OpÁ„o (restante) 3: sair da calculadora
+       case 3: //Op√ß√£o (restante) 3: sair da calculadora
            break;
 
-       default: //Quando o usu·rio digitar um n˙mero n„o previsto
-           printf("\nVocÍ digitou um valor inv·lido!");
+       default: //Quando o usu√°rio digitar um n√∫mero n√£o previsto
+           printf("\nVoc√™ digitou um valor inv√°lido!");
            printf("\n\n\n");
    }
 
@@ -55,44 +55,44 @@ do{ //LaÁo de repetiÁ„o do menu
 
 while (opcao!=3); //Sair da calculadora
     printf("\nClique em qualquer tecla para sair.");
-    printf("\nAtÈ a prÛxima!");
+    printf("\nAt√© a pr√≥xima!");
     printf("\n\n\n");
    return 0;
 }
 
-void conversaoDecBin (int num_decimal) //FunÁ„o convers„o decimal-bin·rio
+void conversaoDecBin (int num_decimal) //Fun√ß√£o convers√£o decimal-bin√°rio
 {
     int dec_binario[64], i=0;
 
-if (num_decimal==0) { //Se o n˙mero decimal for igual a zero, o n˙mero bin·rio tambÈm ser·.
+if (num_decimal==0) { //Se o n√∫mero decimal for igual a zero, o n√∫mero bin√°rio tamb√©m ser√°.
     printf("%d", num_decimal);
     printf("\n\n\n");
-    return;//Finaliza condiÁ„o
+    return;//Finaliza condi√ß√£o
     }
     else {
-        while (num_decimal>0){ //Enquanto o n˙mero decimal for maior que zero...
-           dec_binario[i] = num_decimal%2; // ...entra no laÁo de repetiÁ„o, obtendo-se o valor do resto da divis„o do n˙mero decimal por 2;
-           num_decimal = num_decimal/2; // Divide-se o n˙mero decimal por 2, e inicia o prÛximo loop
+        while (num_decimal>0){ //Enquanto o n√∫mero decimal for maior que zero...
+           dec_binario[i] = num_decimal%2; // ...entra no la√ßo de repeti√ß√£o, obtendo-se o valor do resto da divis√£o do n√∫mero decimal por 2;
+           num_decimal = num_decimal/2; // Divide-se o n√∫mero decimal por 2, e inicia o pr√≥ximo loop
            i++; //Incremento do contador do vetor
            }
-        for (i=i-1; i>=0; i--){//Decremento do vetor do m·ximo a 0, possibilitando a impress„o dos restos de tr·s para frente, formando o n˙mero bin·rio
+        for (i=i-1; i>=0; i--){//Decremento do vetor do m√°ximo a 0, possibilitando a impress√£o dos restos de tr√°s para frente, formando o n√∫mero bin√°rio
                printf("%d", dec_binario[i]);
             }
          printf("\n\n\n");
          return; //Finaliza loop
         }
 }
-void conversaoBinDec (char num_binario[64]) //FunÁ„o convers„o decimal-bin·rio
+void conversaoBinDec (char num_binario[64]) //Fun√ß√£o convers√£o decimal-bin√°rio
 {
     int bin_decimal=0, j=0;
-    for (j=0; j<strlen(num_binario); j++)//LaÁo de repetiÁ„o para validar apenas valores bin·rios.
-    if(num_binario[j]!='1' && num_binario[j]!= '0') { //Se o n˙mero digitado for diferente de 1 e 0, imprime que o n˙mero n„o È bin·rio na tela
-        printf("VocÍ n„o digitou um n˙mero Bin·rio!\n\n\n");
-        return; //Finaliza o laÁo
+    for (j=0; j<strlen(num_binario); j++)//La√ßo de repeti√ß√£o para validar apenas valores bin√°rios
+    if(num_binario[j]!='1' && num_binario[j]!= '0') { //Se o n√∫mero digitado for diferente de 1 e 0, imprime que o n√∫mero n√£o √© bin√°rio na tela
+        printf("Voc√™ n√£o digitou um n√∫mero Bin√°rio!\n\n\n");
+        return; //Finaliza o la√ßo
     }
-    for (j=strlen(num_binario)-1; j>=0; j--) //LaÁo de repetiÁ„o para obter o n˙mero em bin·rio, analisando o n˙mero de tr·s para frente
+    for (j=strlen(num_binario)-1; j>=0; j--) //La√ßo de repeti√ß√£o para obter o n√∫mero em bin√°rio, analisando o da direita para a esquerda
         if (num_binario[j]=='1') //Quando houver o valor 1
-        bin_decimal=bin_decimal+1*pow(2,strlen(num_binario)-1-j); //Efetua o c·lculo
-        printf("N˙mero Decimal:\n%d\n\n\n", bin_decimal);
+        bin_decimal=bin_decimal+1*pow(2,strlen(num_binario)-1-j); //Efetua o c√°lculo
+        printf("N√∫mero Decimal:\n%d\n\n\n", bin_decimal);
         return;
 }
